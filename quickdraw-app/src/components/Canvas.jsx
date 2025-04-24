e58fd16cfad92600a88be20a057fa10d4b30e9ca
mintdexdev <dexdevpersona@gmail.com>
2025-04-24 19:52:30 +0530

    feature: added resizing option, code refactor

--- quickdraw-app/src/components/Canvas.jsx
+++ quickdraw-app/src/components/Canvas.jsx
@@ -3,54 +3,144 @@ import rough from 'roughjs';
 
 let generator = rough.generator();
 
-function generateSeed() {
-  return Math.floor(Math.random() * (2 ** 31 - 1)) + 1;
-}
-
-function createElement(id, properties, type) {
-  const prop1 = {
-    strokeWidth: 1,
-    seed: generateSeed(),
-  }
-  const prop2 = {
+function createElement(id, type, x1, y1, x2, y2) {
+  const roughProperties = {
     strokeWidth: 3,
-    seed: 3,
-  }
-  const prop3 = {
-    strokeWidth: 5,
-    seed: 3,
-  }
-  // const slop
-
-  const props = {
-    ...prop3,
     stroke: 'lightcoral',
     roughness: 0,
     bowing: 3,
     // disableMultiStroke: true
   };
 
-  const { x1, y1, x2, y2, width, height } = properties;
-
   let roughElement = {};
+  const height = y2 - y1;
+  const width = x2 - x1;
 
   if (type === "line") {
-    roughElement = generator.line(x1, y1, x2, y2, props);
+    roughElement = generator.line(x1, y1, x2, y2, roughProperties);
   } else if (type === "rectangle") {
-    const rectWidth = x2 - x1;
-    const rectHeight = y2 - y1;
-    roughElement = generator.rectangle(x1, y1, rectWidth, rectHeight, props);
+    roughElement = generator.rectangle(x1, y1, width, height, roughProperties);
   } else if (type === "ellipse") {
     const centerX = (x2 + x1) / 2;
     const centerY = (y2 + y1) / 2;
-    roughElement = generator.ellipse(centerX, centerY, width, height, props);
+    roughElement = generator.ellipse(centerX, centerY, width, height, roughProperties);
   }
-  return { id, properties, roughElement, type };
+  return { id, type, x1, y1, x2, y2, roughElement };
 }
 
 
 // --------------------------------------
+// when no shape is selected check for click on line of shape
+function positionOnElement(mx, my, element, threshold = 5) {
+  const { type, x1, y1, x2, y2 } = element;
 
+  if (type == "rectangle") {
+    const topLeft = nearPoint(mx, my, x1, y1, "tl");
+    const topRight = nearPoint(mx, my, x2, y1, "tr");
+    const bottomLeft = nearPoint(mx, my, x1, y2, "bl");
+    const bottomRight = nearPoint(mx, my, x2, y2, "br");
+
+    const nearLeft = Math.abs(mx - x1) <= threshold && my >= y1 && my <= y2;
+    const nearRight = Math.abs(mx - x2) <= threshold && my >= y1 && my <= y2;
+    const nearTop = Math.abs(my - y1) <= threshold && mx >= x1 && mx <= x2;
+    const nearBottom = Math.abs(my - y2) <= threshold && mx >= x1 && mx <= x2;
+
+    const onShape = nearLeft || nearRight || nearTop || nearBottom ? "onShape" : null;
+
+    return topLeft || topRight || bottomLeft || bottomRight || onShape;
+
+  } else if (type == "line") {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x: mx, y: my };
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+
+    const start = nearPoint(mx, my, x1, y1, "start");
+    const end = nearPoint(mx, my, x2, y2, "end");
+
+    const onShape = Math.abs(offset) < 1 ? "onShape" : null;
+
+    return start || end || onShape;
+  }
+};
+function distance(a, b) {
+  return (
+    Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
+  );
+}
+function nearPoint(x, y, x1, y1, name) {
+  return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
+}
+
+// when an shape is already selected and not a line
+// function isMouseWithinElement(mx, my, element, threshold = 5) {
+//   const { type, properties } = element;
+//   const { x1, y1, x2, y2 } = properties;
+//   if (type == "rectangle") {
+//     const minX = Math.min(x1, x2);
+//     const minY = Math.min(y1, y2);
+//     const maxX = Math.max(x1, x2);
+//     const maxY = Math.max(y1, y2);
+//     return mx >= minX && mx <= maxX && my >= minY && my <= maxY;
+//   }
+// }
+
+// ------------------------------------------------------------------------
+
+function adjustCoordinates(element) {
+  const { type, x1, y1, x2, y2 } = element;
+
+  const minX = Math.min(x1, x2);
+  const minY = Math.min(y1, y2);
+  const maxX = Math.max(x1, x2);
+  const maxY = Math.max(y1, y2);
+
+  if (type === "rectangle") {
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY, };
+  } else if (type === "line") {
+    if ((x1 < x2 && y1 > y2) || (x1 > x2 && y1 < y2)) {
+      return { x1: minX, y1: maxY, x2: maxX, y2: minY };
+    }
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY, };
+  }
+}
+
+function cursorForPosition(position) {
+  switch (position) {
+    case "tl":
+    case "br":
+      return "nwse-resize"
+    case "tr":
+    case "bl":
+      return "nesw-resize"
+    case "start":
+    case "end":
+      return "pointer"
+    default:
+      return "move";
+  }
+}
+
+const resizedCoordinates = (clientX, clientY, position, coordiantes) => {
+  const { x1, y1, x2, y2 } = coordiantes;
+
+  switch (position) {
+    case "tl":
+      return { x1: clientX, y1: clientY, x2, y2 }
+    case "tr":
+      return { x1, y1: clientY, x2: clientX, y2 }
+    case "bl":
+      return { x1: clientX, y1, x2, y2: clientY }
+    case "br":
+      return { x1, y1, x2: clientX, y2: clientY }
+    case "start":
+      return { x1: clientX, y1: clientY, x2, y2 }
+    case "end":
+      return { x1, y1, x2: clientX, y2: clientY }
+    default:
+      return null; // will not get here
+  }
+}
 
 function Canvas() {
   const canvasRef = useRef(null);
@@ -61,9 +151,9 @@ function Canvas() {
   const [tool, setTool] = useState("line");
 
   //  all shapes drawn in canvas (for now)
-  const [allElements, setAllElements] = useState([]);
+  const [elements, setElements] = useState([]);
   // currently selected element while moving (for now)
-  const [selectedElement, setSelectedElement] = useState(null);
+  const [selectionElement, setSelectionElement] = useState(null);
 
   const [canvasSize, setCanvasSize] = useState({
     width: window.innerWidth,
@@ -77,8 +167,8 @@ function Canvas() {
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
     const roughCanvas = rough.canvas(canvas);
 
-    allElements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
-  }, [canvasSize, allElements, action]);
+    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
+  }, [canvasSize, elements, action]);
 
   //on window resize update canvas
   useEffect(() => {
@@ -95,51 +185,18 @@ function Canvas() {
     };
   }, []);
 
-
-  //functions
-  function isWithinElement(mx, my, element, threshold = 5) {
-    function distance(a, b) {
-      return (
-        Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2))
-      );
-    }
-    const { type } = element;
-    const { x1, y1, x2, y2 } = element.properties;
-    if (type == "rectangle") {
-      // const minX = Math.min(x1, x2);
-      // const minY = Math.min(y1, y2);
-      // const maxX = Math.max(x1, x2);
-      // const maxY = Math.max(y1, y2);
-      // return x >= minX && x <= maxX && y >= minY && y <= maxY;
-
-      const left = Math.min(x1, x2);
-      const right = Math.max(x1, x2);
-      const top = Math.min(y1, y2);
-      const bottom = Math.max(y1, y2);
-
-      const nearLeft = Math.abs(mx - left) <= threshold && my >= top && my <= bottom;
-      const nearRight = Math.abs(mx - right) <= threshold && my >= top && my <= bottom;
-      const nearTop = Math.abs(my - top) <= threshold && mx >= left && mx <= right;
-      const nearBottom = Math.abs(my - bottom) <= threshold && mx >= left && mx <= right;
-
-      return nearLeft || nearRight || nearTop || nearBottom;
-    } else if (type == "line") {
-      const a = { x: x1, y: y1 };
-      const b = { x: x2, y: y2 };
-      const c = { x: mx, y: my };
-      const offset = distance(a, b) - (distance(a, c) + distance(b, c));
-      return Math.abs(offset) < threshold / threshold;
-    }
-  };
+  // ------------------------------------------------------------------------
 
   function getElementAtPosition(x, y) {
-    return allElements.find(element => isWithinElement(x, y, element))
+    return elements
+      .map(element => ({ ...element, position: positionOnElement(x, y, element) }))
+      .find(element => element.position !== null);
   }
 
-  // update any element
-  function updateElement(id, properties, type) {
-    const updatedElement = createElement(id, properties, type);
-    setAllElements(pre => pre.map((elm, i) => i === id ? updatedElement : elm));
+  // update Shape
+  function updateElement(id, type, x1, y1, x2, y2) {
+    const updatedElement = createElement(id, type, x1, y1, x2, y2);
+    setElements(pre => pre.map((elm, i) => i === id ? updatedElement : elm));
   }
 
   // mouse down
@@ -148,26 +205,24 @@ function Canvas() {
 
     if (tool === "selection") {
       const element = getElementAtPosition(clientX, clientY);
+
       if (element) {
-        const offset = {
-          offsetX: clientX - element.properties.x1,
-          offsetY: clientY - element.properties.y1
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+
+        setSelectionElement({ ...element, offsetX, offsetY });
+
+        if (element.position === "onShape") {
+          setAction("moving");
+        } else {
+          setAction("resizing");
         }
-        console.log(element);
-        console.log(offset);
-        setSelectedElement({ ...element, offset });
-        setAction("moving");
       }
-    } else {
-      const id = allElements.length;
-      const properties = {
-        x1: clientX,
-        y1: clientY,
-        x2: clientX,
-        y2: clientY,
-      }
-      const element = createElement(id, properties, tool);
-      setAllElements(previousState => [...previousState, element]);
+    } else { // when tool not selection
+      const id = elements.length;
+      const element = createElement(id, tool, clientX, clientY, clientX, clientY);
+      setElements(previousState => [...previousState, element]);
+      // setSelectionElement(element);
       setAction("drawing");
     }
   }
@@ -175,47 +230,65 @@ function Canvas() {
   // mouse move
   function handleMouseMove(event) {
     const { clientX, clientY } = event;
-    if (action === "moving") {
-      const { id, properties, type, offset } = selectedElement;
-      const { x1, y1, x2, y2, width, height } = properties;
-      const { offsetX, offsetY } = offset;
 
-      const newX = clientX - offsetX;
-      const newY = clientY - offsetY;
-      const updatedCoordinate = {
-        x1: newX,
-        y1: newY,
-        x2: newX + width,
-        y2: newY + height
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY);
+
+      if (element) {
+        event.target.style.cursor = cursorForPosition(element.position);
+      } else {
+        event.target.style.cursor = "default";
       }
-      updateElement(id, updatedCoordinate, type);
     }
-    else if (action === "drawing") {
-      const id = allElements.length - 1;
-      const { x1, y1 } = allElements[id].properties;
 
-      const updatedCoordinate = {
-        x1: x1,
-        y1: y1,
-        x2: clientX,
-        y2: clientY,
-        width: Math.abs(clientX - x1),
-        height: Math.abs(clientY - y1),
-      }
-      updateElement(id, updatedCoordinate, tool);
+    const id = elements.length - 1;
+    if (action === "drawing") {
+      const { type, x1, y1 } = elements[id];
+
+      updateElement(id, type, x1, y1, clientX, clientY);
+    } else if (action === "moving") {
+      const { id, type, x1, y1, x2, y2, offsetX, offsetY } = selectionElement;
+
+      const width = x2 - x1;
+      const height = y2 - y1;
+
+      const newX1 = clientX - offsetX;
+      const newY1 = clientY - offsetY;
+      const newX2 = newX1 + width;
+      const newY2 = newY1 + height;
+
+      updateElement(id, type, newX1, newY1, newX2, newY2);
+
+    } else if (action === "resizing") {
+      const { id, type, position, ...coordiantes } = selectionElement;
+      const { x1, y1, x2, y2 } = resizedCoordinates(clientX, clientY, position, coordiantes);
+      updateElement(id, type, x1, y1, x2, y2);
     }
   }
 
   // mouse up
-  function handleMouseUp(event) {
-    const index = allElements.length - 1;
-    const { x1, y1, x2, y2 } = allElements[index].properties;
+  function handleMouseUp() {
+    const index = elements.length - 1;
+    const { id, type, x1, y1, x2, y2 } = elements[index];
+
+    if (action === "drawing") {
+      const { x1, y1, x2, y2 } = adjustCoordinates(elements[index]);
+
+      updateElement(id, type, x1, y1, x2, y2);
+    } else
+      if (action === "resizing") {
+        const index = selectionElement.id;
+        const { x1, y1, x2, y2 } = adjustCoordinates(elements[index]);
+
+        updateElement(id, type, x1, y1, x2, y2);
+      }
+
     if (x1 === x2 && y1 === y2) {
-      setAllElements(prev => prev.filter((elm, i) => i !== index));
+      setElements(prev => prev.filter((elm, i) => i !== id));
     }
 
     setAction("none");
-    setSelectedElement(null);
+    setSelectionElement(null);
   }
 
   return (
